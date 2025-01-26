1ae1656aca5ffa8b59030b1b5e07b0b81a1c3d94
Ahnzi <42331481+ahnzi91@users.noreply.github.com>
2025-01-26 15:34:24 +0900

    1.4) 자바스크립트 실습 환경 설정하기 - ✨feat: 수강 완료

--- section01/chapter04.js
+++ section01/chapter04.js
@@ -1,1 +1,27 @@
-console.log("chapter 4");
+// 1. 변수
+let age = 27;
+console.log(age); // 27
+
+age = 30;
+console.log(age); // 30
+
+let address;
+console.log(address); // undefined
+
+// 2. 상수
+const birth = "2025.01.01";
+birth = 123;
+
+// 3. 변수 명명 규칙 (네이밍 규칙)
+// 3-1. $, _ 두 기호를 제외한 기호는 변수명에 사용할 수 없다.
+// let #name = "hello";
+let $_name = "hello";
+
+// 3-2. 숫자로 시작할 수 없다.
+// let 2name;
+let name1;
+let $2name;
+
+// 3-3. 예약어를 사용할 수 없다.
+// let let;
+// let if;
